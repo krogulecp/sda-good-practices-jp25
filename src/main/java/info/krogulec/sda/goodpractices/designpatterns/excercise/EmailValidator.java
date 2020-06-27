805fc003ba44a4f68341ff7e55aa151f44e338e8
Piotr Krogulec <krogulec.piotr@gmail.com>
2020-06-27 14:23:55 +0200

    template-method

---
+++ src/main/java/info/krogulec/sda/goodpractices/designpatterns/excercise/EmailValidator.java
@@ -0,0 +1,14 @@
+package info.krogulec.sda.goodpractices.designpatterns.excercise;
+
+//TODO
+public class EmailValidator {
+    public static EmailValidator getInstance() {
+        //TODO
+        return null;
+    }
+
+    public boolean isValid(String email) {
+        //TODO
+        return false;
+    }
+}
